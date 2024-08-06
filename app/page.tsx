2ff002bd03be4a175b0a400945b1ea2dd5040ea3
Ryan <ryan.figueredo059@gmail.com>
2024-08-05 23:55:28 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import Image from "next/image"
 
 const Home = () => {
@@ -28,9 +31,28 @@ const Home = () => {
           />
         </div>
 
-        <div className="mt-6">
-          <h3 className="uppercase text-gray-500">Agendamentos</h3>
-        </div>
+        <h3 className="mt-6 uppercase text-gray-500">Agendamentos</h3>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 px-5">
+              <p className="text-sm">Fevereiro</p>
+              <p className="text-2xl">06</p>
+              <p className="text-sm">09:45</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
