f711ba73cf0ba3efbd828e9a2a0d597c32926d12
Ryan <ryan.figueredo059@gmail.com>
2024-08-06 00:31:52 -0300

    feat: add barbershop recommended

---
+++ app/_components/barbershop-item.tsx
@@ -0,0 +1,35 @@
+import { Barbershop } from "@prisma/client"
+import { Card, CardContent } from "./ui/card"
+import Image from "next/image"
+import { Button } from "./ui/button"
+
+interface BarbershopItemProps {
+  barbershop: Barbershop
+}
+
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+  return (
+    <Card className="min-w-[167px] rounded-2xl">
+      <CardContent className="p-0 px-1 pt-1">
+        <div className="relative h-[159px] w-full">
+          <Image
+            alt={barbershop.name}
+            fill
+            className="rounded-2xl object-cover"
+            src={barbershop.imageUrl}
+          />
+        </div>
+
+        <div className="px-1 py-3">
+          <h3 className="truncate font-semibold">{barbershop.name}</h3>
+          <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
+          <Button variant="secondary" className="mt-3 w-full">
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default BarbershopItem
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,13 @@ import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import Image from "next/image"
+import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/barbershop-item"
 
-const Home = () => {
+const Home = async () => {
+  const barbershops = await db.barbershop.findMany({})
+  console.log(barbershops)
+
   return (
     <div>
       <Header />
@@ -31,9 +36,9 @@ const Home = () => {
           />
         </div>
 
-        <h3 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Agendamentos
-        </h3>
+        </h2>
 
         <Card>
           <CardContent className="flex justify-between p-0">
@@ -55,6 +60,16 @@ const Home = () => {
             </div>
           </CardContent>
         </Card>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   )
