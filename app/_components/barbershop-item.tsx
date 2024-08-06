93464c4ed48061b5a7828b5dbc63606fdfd4351c
Ryan <ryan.figueredo059@gmail.com>
2024-08-06 00:37:44 -0300

    feat: add barbershop recommended two

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -2,6 +2,8 @@ import { Barbershop } from "@prisma/client"
 import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
 import { Button } from "./ui/button"
+import { Badge } from "./ui/badge"
+import { StarIcon } from "lucide-react"
 
 interface BarbershopItemProps {
   barbershop: Barbershop
@@ -18,6 +20,13 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
             className="rounded-2xl object-cover"
             src={barbershop.imageUrl}
           />
+          <Badge
+            className="absolute left-2 top-2 space-x-1"
+            variant="secondary"
+          >
+            <StarIcon size={12} className="fill-primary text-primary" />
+            <p className="text-xs font-semibold">5,0</p>
+          </Badge>
         </div>
 
         <div className="px-1 py-3">
