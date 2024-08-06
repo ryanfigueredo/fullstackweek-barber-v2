91d98dcb270ad3e2c141ddb3e65c4d53044e297f
Ryan <ryan.figueredo059@gmail.com>
2024-08-05 23:39:44 -0300

    feat: add header, and header components, and banner

---
+++ app/_components/header.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Card>
+        <CardContent className="flex flex-row items-center justify-between p-5">
+          <Image alt="FSW Barber" src="/logo.svg" height={18} width={120} />
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </CardContent>
+      </Card>
+    </header>
+  )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,39 @@
-"use client"
-
-import { useState } from "react"
-import { Button } from "@/app/_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
-  const [] = useState()
-  return <Button>test</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Ryan!</h2>
+        <p>Segunda-Feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua Busca" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            className="rounded-xl object-cover"
+            src="/banner-01.png"
+            fill
+          />
+        </div>
+
+        <div className="mt-6">
+          <h3 className="uppercase text-gray-500">Agendamentos</h3>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
