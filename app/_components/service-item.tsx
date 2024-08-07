1a15940ea60c47496cee420f2587d4559655f4e9
Ryan <ryan.figueredo059@gmail.com>
2024-08-06 21:30:35 -0300

    feat: ensure barbershop services are shown on barbershop page

---
+++ app/_components/service-item.tsx
@@ -0,0 +1,42 @@
+import type { BarbershopService } from "@prisma/client"
+import Image from "next/image"
+import { Button } from "./ui/button"
+import { Card, CardContent } from "./ui/card"
+
+interface ServiceItemProps {
+  service: BarbershopService
+}
+
+const ServiceItem = ({ service }: ServiceItemProps) => {
+  return (
+    <Card>
+      <CardContent className="flex items-center gap-3 p-3">
+        <div className="max-w-[110px]- relative max-h-[110px] min-h-[110px] min-w-[110px]">
+          <Image
+            alt={service.name}
+            src={service.imageUrl}
+            className="rounded-lg object-cover"
+            fill
+          />
+        </div>
+        <div className="space-y-2">
+          <h3 className="text-sm font-semibold">{service.name}</h3>
+          <p className="text-sm text-gray-400">{service.description}</p>
+          <div className="flex items-center justify-between">
+            <p className="text-sm font-bold text-primary">
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(Number(service.price))}
+            </p>
+            <Button size="sm" variant="secondary">
+              Reservar
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default ServiceItem
