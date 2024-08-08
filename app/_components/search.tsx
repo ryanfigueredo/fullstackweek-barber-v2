3b4d7e594272a07770c79fef438be8aaa14861a4
Ryan <ryan.figueredo059@gmail.com>
2024-08-08 18:52:37 -0300

    feat: add tool search

---
+++ app/_components/search.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import { Button } from "./ui/button"
+import { SearchIcon } from "lucide-react"
+import { Input } from "./ui/input"
+import { useRouter } from "next/navigation"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form"
+
+// estudar zod * validar dados
+const formSchema = z.object({
+  search: z.string().trim().min(1, {
+    message: "Digite algo para buscar",
+  }),
+})
+
+const Search = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      search: "",
+    },
+  })
+  const router = useRouter()
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    router.push(`/barbershops?search=${data.search}`)
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="flex gap-2">
+        <FormField
+          control={form.control}
+          name="search"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormControl>
+                <Input
+                  placeholder="Faça sua Busca"
+                  {...field}
+                  className="w-full"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">
+          <SearchIcon />
+        </Button>
+      </form>
+    </Form>
+  )
+}
+
+export default Search
