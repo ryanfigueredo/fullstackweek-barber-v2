4467f0f9a36e50dc79f2ff53786925dce6d83fa1
Ryan <ryan.figueredo059@gmail.com>
2024-08-08 19:15:41 -0300

    feat: ensure barbershops can be searched by title and service

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -22,12 +22,15 @@ const SidebarSheet = () => {
   const handleLoginWithGoogleClick = () => signIn("google")
   const handleLogoutClick = () => signOut()
 
+  console.log(data?.user)
+
   return (
-    <SheetContent>
+    <SheetContent className="overflow-y-auto">
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
-      <div className="flex items-center gap-3 border-b border-solid py-5">
+
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
         {data?.user ? (
           <div className="flex items-center gap-2">
             <Avatar>
@@ -55,13 +58,14 @@ const SidebarSheet = () => {
                     Conecte-se usando sua conta do Google.
                   </DialogDescription>
                 </DialogHeader>
+
                 <Button
                   variant="outline"
                   className="gap-1 font-bold"
                   onClick={handleLoginWithGoogleClick}
                 >
                   <Image
-                    alt="Faça seu login com o Google"
+                    alt="Fazer login com o Google"
                     src="/google.svg"
                     width={18}
                     height={18}
@@ -73,7 +77,8 @@ const SidebarSheet = () => {
           </>
         )}
       </div>
-      <div className="flex flex-col gap-2 border-b border-solid p-5 py-5">
+
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
         <SheetClose asChild>
           <Button className="justify-start gap-2" variant="ghost" asChild>
             <Link href="/">
@@ -87,27 +92,33 @@ const SidebarSheet = () => {
           Agendamentos
         </Button>
       </div>
-      <div className="flex flex-col gap-2 border-b border-solid p-5 py-5">
+
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
         {quickSearchOptions.map((option) => (
-          <Button
-            className="justify-start gap-2"
-            key={option.title}
-            variant="ghost"
-          >
-            <Image
-              src={option.imageUrl}
-              height={18}
-              width={18}
-              alt={option.title}
-            />
-            {option.title}
-          </Button>
+          <SheetClose key={option.title} asChild>
+            <Button className="justify-start gap-2" variant="ghost" asChild>
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  alt={option.title}
+                  src={option.imageUrl}
+                  height={18}
+                  width={18}
+                />
+                {option.title}
+              </Link>
+            </Button>
+          </SheetClose>
         ))}
       </div>
-      <div className="flex flex-col gap-2 p-5 py-5">
-        <Button variant="ghost" className="justify-start gap-2">
-          <LogOutIcon size={18} onClick={handleLogoutClick} />
-          Sair da Conta
+
+      <div className="flex flex-col gap-2 py-5">
+        <Button
+          variant="ghost"
+          className="justify-start gap-2"
+          onClick={handleLogoutClick}
+        >
+          <LogOutIcon size={18} />
+          Sair da conta
         </Button>
       </div>
     </SheetContent>
